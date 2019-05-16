389f1b89c1cb3df4b8eed68e773722caa0592905
Butramyou Dmitriy <d.butramyou@gmail.com>
2019-05-17 00:19:07 +0300

    Добавлены связи между таблицами базы данных (one to many)

--- WebApplication/src/main/java/com/example/web/controller/RegistrationController.java
+++ WebApplication/src/main/java/com/example/web/controller/RegistrationController.java
@@ -22,11 +22,11 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> mogel) {
+    public String addUser(User user, Map<String, Object> model) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
         if(userFromDb != null) {
-            mogel.put("message", "User exists");
+            model.put("message", "User exists");
             return "registration";
         }
 
